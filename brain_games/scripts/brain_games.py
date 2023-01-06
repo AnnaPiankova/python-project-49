ee214ebe1d68b66c81294df40a46b5692c168ed9
Anna Piankova <anny.pyankova@gmail.com>
2023-01-06 13:30:16 +0100

    changes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,9 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    print(f'Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
