a67e078c3ac60e12c2c72fb6a6c1a826af982fa6
Anna Piankova <anny.pyankova@gmail.com>
2023-02-06 17:34:25 +0100

    updates

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -14,7 +14,7 @@ def get_progression(start, step, length_of_prog):
 def quiz():
     length_of_prog = 10
     miss_number_index = randint(1, length_of_prog - 1)
-    step = randint(0, 10)
+    step = randint(1, 10)
     start = randint(0, 10)
     progression = get_progression(start, step, length_of_prog)
     answer = progression.pop(miss_number_index)
