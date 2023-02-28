3b5abe8ad1f7a957063dfc48dafb12f9af9e4182
Anna Piankova <anny.pyankova@gmail.com>
2023-02-28 16:01:45 +0100

    updates

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,19 +1,14 @@
 from random import randint
-from brain_games.core import core_game
 
 
-start_game = 'Answer "yes" if the number is even, otherwise answer "no".'
+START_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(number):
     return number % 2 == 0
 
 
-def quiz():
+def print_question():
     question = randint(0, 100)
     answer = "yes" if is_even(question) else "no"
-    return question, answer
-
-
-def run_game():
-    core_game(quiz, start_game)
+    return question, str(answer)
