5b962eb7ffd3c13d3f6f7aef55b993e7a6e9fe3e
Anna Piankova <anny.pyankova@gmail.com>
2023-02-28 16:02:23 +0100

    updates

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,8 +1,7 @@
 from random import randint
-from brain_games.core import core_game
 
 
-start_game = 'Find the greatest common divisor of given numbers.'
+START_GAME = 'Find the greatest common divisor of given numbers.'
 
 
 def get_gcd(num1, num2):
@@ -13,13 +12,9 @@ def get_gcd(num1, num2):
         divisor -= 1
 
 
-def quiz():
+def print_question():
     num1 = randint(0, 50)
     num2 = randint(0, 50)
     question = f'{num1} {num2}'
     answer = get_gcd(num1, num2)
     return question, str(answer)
-
-
-def run_game():
-    core_game(quiz, start_game)
