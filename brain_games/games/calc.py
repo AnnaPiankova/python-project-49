16477558e4162187abe5097667b6467403a42957
Anna Piankova <anny.pyankova@gmail.com>
2023-02-28 15:59:47 +0100

    change function

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,28 +1,23 @@
 from random import choice, randint
-from brain_games.core import core_game
 
 
-start_game = 'What is the result of the expression?'
-sign = choice(['+', '-', '*'])
+START_GAME = 'What is the result of the expression?'
 
 
-def get_expression(num_1, num_2, sign):
-    if sign == '+':
-        result = num_1 + num_2
-    elif sign == '-':
-        result = num_1 - num_2
-    else:
-        result = num_1 * num_2
-    return result
+def calculate(num_1, num_2, sign):
+    match sign:
+        case "+":
+            return num_1 + num_2
+        case "-":
+            return num_1 - num_2
+        case "*":
+            return num_1 * num_2
 
 
-def quiz():
+def print_question():
+    sign = choice(['+', '-', '*'])
     num1 = randint(0, 50)
     num2 = randint(0, 50)
-    answer = get_expression(num1, num2, sign)
+    answer = calculate(num1, num2, sign)
     question = f'{num1} {sign} {num2}'
     return question, str(answer)
-
-
-def run_game():
-    core_game(quiz, start_game)
